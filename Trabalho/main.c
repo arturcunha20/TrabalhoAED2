41c38988532b7de5d78adf46f167c7e160ea9e55
arturcunha20 <76968989+arturcunha20@users.noreply.github.com>
2021-04-24 13:56:28 +0100

    Add files via upload

---
+++ Trabalho/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#define SEPARADOR -14
+#define SEPARADORNAME -50
+#define SEPARADORID -6
+
+typedef struct {
+    char part_num[20];
+    char name[100];
+    char class[50];
+    int stock;
+}Parts;
+
+void ReadParts()
+{
+    FILE *file;
+    Parts p;
+
+    file = fopen("../Files/parts.tsv","r");
+    int x=0;
+    while (!feof(file))
+    {
+        if(x == 1)
+        {
+            fscanf(file,"%[^\t]%[^\t]%[^\t]%[^\n]",&p.part_num, &p.name, &p.class, &p.stock);
+            printf("%*s | %*s | %*s | %*d\n",
+                   SEPARADOR, p.part_num,
+                   SEPARADORNAME, p.name,
+                   SEPARADOR, p.class,
+                   SEPARADOR, p.stock
+            );
+        }
+        x++;
+    }
+
+    printf("\n");
+    fclose(file);
+}
+
+
+int main() {
+    ReadParts();
+    return 0;
+}
