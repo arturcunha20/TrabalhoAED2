2f8acc238f2baaba06d5420488d2fc639bb40ebf
arturcunha20 <arturcunhagpsi1720@gmail.com>
2021-04-24 13:58:37 +0100

    .

--- Trabalho/cmake-build-debug/CMakeFiles/Trabalho.dir/DependInfo.cmake
+++ Trabalho/cmake-build-debug/CMakeFiles/Trabalho.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "C:/Users/Artur/Desktop/-/escola/1 Ano - 2 Semestre/AED/Trabalho pratico/Trabalho/main.c" "C:/Users/Artur/Desktop/-/escola/1 Ano - 2 Semestre/AED/Trabalho pratico/Trabalho/cmake-build-debug/CMakeFiles/Trabalho.dir/main.c.obj"
+  "C:/Users/Artur/Documents/GitHub/TrabalhoAED2/Trabalho/main.c" "C:/Users/Artur/Documents/GitHub/TrabalhoAED2/Trabalho/cmake-build-debug/CMakeFiles/Trabalho.dir/main.c.obj"
   )
 set(CMAKE_C_COMPILER_ID "GNU")
 
--- Trabalho/cmake-build-debug/cmake_install.cmake
+++ Trabalho/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Artur/Desktop/-/escola/1 Ano - 2 Semestre/AED/Trabalho pratico/Trabalho
+# Install script for directory: C:/Users/Artur/Documents/GitHub/TrabalhoAED2/Trabalho
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Artur/Desktop/-/escola/1 Ano - 2 Semestre/AED/Trabalho pratico/Trabalho/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Artur/Documents/GitHub/TrabalhoAED2/Trabalho/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
